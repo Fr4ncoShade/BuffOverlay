e642f655b23893a85ff24a0db3b3961d3be9ecf8
B <159943278+Fr4ncoShade@users.noreply.github.com>
2026-05-28 15:53:02 +0300

    Update default spell database

--- BuffOverlay/Spells/Spells.lua
+++ BuffOverlay/Spells/Spells.lua
@@ -17,9 +17,12 @@ BuffOverlay.defaultSpells = {
     [48707] = { class = "DEATHKNIGHT", prio = 50 }, --Anti-Magic Shell
     [47484] = { class = "DEATHKNIGHT", prio = 50 }, --Huddle (Ghoul)
     [48792] = { class = "DEATHKNIGHT", prio = 50 }, --Icebound Fortitude
-    [50461] = { class = "DEATHKNIGHT", prio = 50 }, --Anti-Magic Zone
+    [51052] = { class = "DEATHKNIGHT", prio = 50 }, --Anti-Magic Zone
+	[49016] = { class = "DEATHKNIGHT", prio = 50 }, --Unholy Frenzy
+	[55233] = { class = "DEATHKNIGHT", prio = 50 }, --Vampiric Blood
 
     -- Druid
+	[50334] = { class = "DRUID", prio = 40 },  --Berserk
     [22812] = { class = "DRUID", prio = 50 }, --Barkskin
     [22842] = { class = "DRUID", prio = 50 }, --Frenzied Regeneration
     [61336] = { class = "DRUID", prio = 50 }, --Survival Instincts
@@ -32,6 +35,8 @@ BuffOverlay.defaultSpells = {
     [26064] = { class = "HUNTER", prio = 50 }, --Shell Shield (Pet)
     [53476] = { class = "HUNTER", prio = 50 }, --Intervene (Pet)
     [53480] = { class = "HUNTER", prio = 50 }, --Roar of Sacrifice (Pet)
+	[19574] = { class = "HUNTER", prio = 50 }, --Bestial Wrath (Pet)
+	
 
     -- Mage
     [45438] = { class = "MAGE", prio = 10 }, --Ice Block
@@ -54,28 +59,34 @@ BuffOverlay.defaultSpells = {
     [1022] = { class = "PALADIN", prio = 10 }, --Blessing of Protection
         [5599] = { parent = 1022 },
         [10278] = { parent = 1022 },
-    [19753] = { class = "PALADIN", prio = 10 }, --Divine Intervention
+    [19752] = { class = "PALADIN", prio = 10 }, --Divine Intervention
     [25771] = { class = "PALADIN", prio = 20 }, --Forbearance
+	[31884] = { class = "PALADIN", prio = 30 }, --Avenging Wrath
     [498] = { class = "PALADIN", prio = 50 },   --Divine Protection
     [31821] = { class = "PALADIN", prio = 50 }, --Aura Mastery
     [31852] = { class = "PALADIN", prio = 50 }, --Ardent Defender
     [64205] = { class = "PALADIN", prio = 50 }, --Divine Sacrifice
+	[6940] = { class = "PALADIN", prio = 50 }, --Hand of Sacrifice
+	[1038] = { class = "PALADIN", prio = 60 }, --Hand of Salvation
+	[54428] = { class = "PALADIN", prio = 80 }, --Divine Plea
 
     -- Priest
     [47788] = { class = "PRIEST", prio = 10 }, --Guardian Spirit
     [27827] = { class = "PRIEST", prio = 10 }, --Spirit of Redemption
+	[10060] = { class = "PRIEST", prio = 10 }, --Power Infusion
     [47585] = { class = "PRIEST", prio = 50 }, --Dispersion
     [33206] = { class = "PRIEST", prio = 50 }, --Pain Suppression
 
     -- Rogue
     [31224] = { class = "ROGUE", prio = 10 }, --Cloak of Shadows
-    [45182] = { class = "ROGUE", prio = 50 }, --Cheating Death
     [5277] = { class = "ROGUE", prio = 50 },  --Evasion
         [26669] = { parent = 5277 },
     [14278] = { class = "ROGUE", prio = 50 }, --Ghostly Strike
     [1784] = { class = "ROGUE", prio = 70 },  --Stealth
+	[57934] = { class = "ROGUE", prio = 70 },  --Tricks of the Trade
 
     -- Shaman
+	[51514] = { class = "SHAMAN", prio = 20 }, --Hex
     [30823] = { class = "SHAMAN", prio = 50 }, --Shamanistic Rage
     [8178] = { class = "SHAMAN", prio = 50 },  --Grounding Totem
 
@@ -90,6 +101,7 @@ BuffOverlay.defaultSpells = {
         [19441] = { parent = 7812 },
         [19442] = { parent = 7812 },
         [19443] = { parent = 7812 },
+	[59672] = { class = "WARLOCK", prio = 50 }, --Metamorphosis
 
     -- Warrior
     [46924] = { class = "WARRIOR", prio = 10 }, --Bladestorm
@@ -101,6 +113,7 @@ BuffOverlay.defaultSpells = {
     [23920] = { class = "WARRIOR", prio = 50 }, --Spell Reflection
 
     -- Racials
+	[26297] = { class = "MISC", prio = 40 }, --Berserking
     [58984] = { class = "MISC", prio = 70 }, --Shadowmeld
 
     -- Misc
